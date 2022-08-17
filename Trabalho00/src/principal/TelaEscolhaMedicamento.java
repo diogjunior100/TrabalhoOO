2a2747d1b8eaa8ca0d0a64a67b9678a6a8a2f16b
diogjunior100 <diogjunior10071@gmail.com>
2022-08-17 13:25:15 -0300

    Arquivos novos de telas

---
+++ Trabalho00/src/principal/TelaEscolhaMedicamento.java
@@ -0,0 +1,53 @@
+package principal;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import controle.*;
+
+public class TelaEscolhaMedicamento implements ActionListener {
+	
+	private static JFrame janela = new JFrame("Controle de Medicamento");
+	private static JLabel titulo = new JLabel("Menu");
+	private static JButton comprimido = new JButton("Comprimido");
+	private static JButton liquido = new JButton("Líquido");
+	
+	
+	
+	public TelaEscolhaMedicamento() {
+		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		titulo.setBounds(170, 10, 150, 30);
+		comprimido.setBounds(220, 70, 140, 100);
+		
+		liquido.setBounds(20, 70, 140, 100);
+		
+		janela.setLayout(null);
+		
+		janela.add(titulo);
+		janela.add(comprimido);
+		janela.add(liquido);
+		
+		
+		janela.setSize(400, 250);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+	}
+	
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		
+		if(src == medicamento)
+			new TelaPessoa().mostrarDados(dados, 1);
+		
+		if(src == prof)
+			new TelaPessoa().mostrarDados(dados, 2);
+		
+		if(src == curso)
+			JOptionPane.showMessageDialog(null, 
+					"Ainda precisam ser implementadas as funcionalidades\n"
+					+ "relacionadas a curso e a matr�cula", null, 
+					JOptionPane.INFORMATION_MESSAGE);
+	}
+}
