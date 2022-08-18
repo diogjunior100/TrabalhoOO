cc6ee89fe3539f85edb947bd6d59fae4e2fb3a17
diogjunior100 <diogjunior10071@gmail.com>
2022-08-18 12:05:04 -0300

    Colocando projeto oficial do trabalho de OO

---
+++ ProjetoControleMedicamentos/src/objetos/Horario.java
@@ -0,0 +1,54 @@
+package objetos;
+
+
+public class Horario {
+	private int hora;
+	private int minuto;
+	private String diadaSemana;
+	private String detalhedoHorario;
+	
+	public Horario(int hora, int minuto, String diadaSemana, String detalhedoHorario) {
+		this.hora = hora;
+		this.minuto = minuto;
+		this.diadaSemana = diadaSemana;
+		this.detalhedoHorario = detalhedoHorario;
+	}
+	
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public int getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+	public String getDiadaSemana() {
+		return diadaSemana;
+	}
+	public void setDiadaSemana(String diadaSemana) {
+		this.diadaSemana = diadaSemana;
+	}
+	public String getDetalhedoHorario() {
+		return detalhedoHorario;
+	}
+	public void setDetalhedoHorario(String detalhedoHorario) {
+		this.detalhedoHorario = detalhedoHorario;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "\n" + "Horario"+ "\n" + 
+				"Hora: " + this.hora + "\n" + 
+				"Minuto: " + this.minuto + "\n" +
+				"Dia da Semana: " + this.diadaSemana + "\n" + 
+				"Detalhe do Horario: " + this.detalhedoHorario + "\n";
+	}
+	
+	
+	
+}
